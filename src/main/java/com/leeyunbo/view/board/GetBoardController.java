e0aaa1f32a002fc14c98a407bd38b9deb7517264
leeyunbo <servers1@naver.com>
2020-05-02 18:33:40 +0900

    [init] controller, handlermapping 개발 및 설정 완료

---
+++ src/main/java/com/leeyunbo/view/board/GetBoardController.java
@@ -0,0 +1,36 @@
+package com.leeyunbo.view.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.leeyunbo.biz.board.BoardVO;
+import com.leeyunbo.biz.board.impl.BoardDAO;
+
+public class GetBoardController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 상세 조회 처리");
+		// 1. 검색할 게시글 번호 추출
+		String seq = request.getParameter("seq");
+
+		// 2. DB 연동 처리 
+		BoardVO vo = new BoardVO(); 
+		vo.setSeq(Integer.parseInt(seq));
+		
+		BoardDAO boardDAO = new BoardDAO();
+		BoardVO board = boardDAO.getBoard(vo);
+		
+		// 3. Session 저장
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession(); 
+		session.setAttribute("board", board);
+		mav.setViewName("getBoard.jsp");
+		return mav;
+	}
+	
+}
---
+++ src/main/java/com/leeyunbo/view/board/GetBoardListController.java
@@ -0,0 +1,36 @@
+package com.leeyunbo.view.board;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.leeyunbo.biz.board.BoardVO;
+import com.leeyunbo.biz.board.impl.BoardDAO;
+
+public class GetBoardListController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 목록 검색 처리");
+		
+		//1. 사용자 입력 정보 추출(검색 기능 이후에 구현) 
+		BoardVO boardVO = new BoardVO();
+		
+		//2. DB 연동 처리 
+		BoardDAO boardDAO = new BoardDAO();
+		List<BoardVO> boardList = boardDAO.getBoardList();
+		
+		
+		//3. 검색 결과를 세션에 저장하고 이동할 화면 이름 리턴, getBoardList.jsp
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession(); 
+		session.setAttribute("boardList", boardList);	
+		mav.setViewName("getBoardList.jsp");
+		return mav;
+	}
+}
